a78e32c98143c32f1eda717a88dbd1df8f8ed225
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2018-11-16 01:22:12 -0500

    Module for opening connection to DB

---
+++ db.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"database/sql"
+)
+
+const (
+	Driver       = "postgres"
+	DBConnection = "user=johnamadeodaniswara dbname=intouch_android sslmode=disable"
+)
+
+func getDBConnection() (*sql.DB, error) {
+	db, err := sql.Open(Driver, DBConnection)
+
+	if err != nil {
+		db.Close()
+		return db, err
+	}
+
+	return db, nil
+}

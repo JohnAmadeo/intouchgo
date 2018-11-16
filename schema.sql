60ae7e3d81e08c4224c285250ab5e85bedf5e24d
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2018-11-16 01:14:20 -0500

    (DB 1) Create schema

---
+++ schema.sql
@@ -0,0 +1,49 @@
+DROP TABLE facilities;
+DROP TABLE inmates;
+DROP TABLE letters;
+
+-- Assumes that drafts are stored on-device and never reach the cloud
+CREATE TABLE facilities (
+    name VARCHAR PRIMARY KEY,
+    address VARCHAR NOT NULL
+);
+
+CREATE TABLE inmates (
+    id VARCHAR PRIMARY KEY,
+    firstName VARCHAR NOT NULL,
+    lastName VARCHAR NOT NULL,
+    inmateNumber VARCHAR,
+    dateOfBirth DATE,
+    facility VARCHAR REFERENCES facilities(name)
+);
+
+CREATE TABLE letters (
+    id VARCHAR PRIMARY KEY,
+    author VARCHAR NOT NULL,
+    recipient VARCHAR NOT NULL REFERENCES inmates(id),
+    subject VARCHAR,
+    text VARCHAR NOT NULL,
+    timeSent DATE NOT NULL,
+    timeLastEdited DATE NOT NULL,
+    isDraft BOOLEAN NOT NULL
+);
+
+INSERT INTO facilities
+    VALUES ('CT Prison 1', '5445 Hardaway Park Drive');
+INSERT INTO facilities 
+    VALUES ('CT Prison 2', '33A Holmes St NW');
+    
+INSERT INTO inmates
+    VALUES ('asdf-123s-ddss', 'John', 'Grant', 'AA123', '05/03/91', 'CT Prison 1');
+    
+INSERT INTO letters
+    VALUES (
+        'sd8f-1239-zxqw', 
+        'jadk157', 
+        'asdf-123s-ddss', 
+        'Merry Christmas!', 
+        'Hi John!',
+        '02/10/18',
+        '02/10/18',
+        false 
+    );

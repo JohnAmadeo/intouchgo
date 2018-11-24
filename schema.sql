7d09337ae4b8ac002f8cb4d5379a8cb4e10bbfaa
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2018-11-23 22:07:38 -0500

    Add facilities to schema
    - Facilities are manually inserted since there only a) a handful of them, b) some of the facilities have pages that indicate they are closed down but the HTML placement is irregular and thus hard to check for

--- schema.sql
+++ schema.sql
@@ -5,6 +5,7 @@ DROP TABLE facilities;
 -- Assumes that drafts are stored on-device and never reach the cloud
 CREATE TABLE facilities (
     name VARCHAR PRIMARY KEY,
+    shortName VARCHAR,
     address VARCHAR NOT NULL CHECK (length(address) > 0)
 );
 
@@ -15,8 +16,7 @@ CREATE TABLE inmates (
     firstName VARCHAR NOT NULL CHECK (length(firstName) > 0),
     lastName VARCHAR NOT NULL CHECK (length(lastName) > 0),
     dateOfBirth DATE,
-    -- facility VARCHAR REFERENCES facilities(name),
-    facility VARCHAR,
+    facility VARCHAR REFERENCES facilities(name),
     active BOOLEAN NOT NULL,
     PRIMARY KEY(state, inmateNumber)
 );
@@ -32,25 +32,18 @@ CREATE TABLE letters (
     isDraft BOOLEAN NOT NULL
 );
 
-INSERT INTO facilities
-    VALUES ('CT Prison 1', '5445 Hardaway Park Drive');
-INSERT INTO facilities 
-    VALUES ('CT Prison 2', '33A Holmes St NW');
-INSERT INTO facilities 
-    VALUES ('CT Prison 3', '7F One Lawrence Way');
-    
-INSERT INTO inmates VALUES
-    ('asdf-123s-ddss', 'CT', 'AA123', 'John', 'Grant', '05/03/91', 'CT Prison 1', true),
-    ('123a-1das-mmji', 'CT', 'AA296', 'Marlene', 'Enrique', '01/05/78', 'CT Prison 2', false);
-    
-INSERT INTO letters
-    VALUES (
-        'sd8f-1239-zxqw', 
-        'jadk157', 
-        'asdf-123s-ddss', 
-        'Merry Christmas!', 
-        'Hi John!',
-        '02/10/18',
-        '02/10/18',
-        false 
-    );
+INSERT INTO facilities VALUES
+    ('Bridgeport Correctional Center', 'Bridgeport CC', '1106 North Avenue, Bridgeport, CT 06604'),
+    ('Brooklyn Correctional Institution', 'Brooklyn CI', '59 Hartford Road, Brooklyn, CT 06234'),
+    ('Cheshire Correctional Institution', 'Cheshire CI', '900 Highland Avenue, Cheshire, CT 06410 '),
+    ('Corrigan-Radgowski Correctional Center', 'Corrigan-Radgowski CC', '986 Norwich-New London Turnpike, Uncasville, CT 06382 '),
+    ('Garner Correctional Institution', 'Garner CI', '50 Nunnawauk Road, Newtown, CT 06470 '),
+    ('Hartford Correctional Center', 'Hartford CC', '177 Weston Street, Hartford, CT 06120 '),
+    ('MacDougall-Walker Correctional Institution', 'MacDougall-Walker CI', '1153 East Street, South Suffield , CT 06080 '),
+    ('Manson Youth Institution', 'Manson YI', '42 Jarvis Street, Cheshire, CT 06410'),
+    ('New Haven Correctional Center', 'New Haven CC', '245 Whalley Avenue, POB 8000, New Haven, CT 06511 '),
+    ('Northern Correctional Institution', 'Northern CI', '287 Bilton Road, POB 665, Somers, CT 06071'),
+    ('Osborn Correctional Institution', 'Osborn CI', '335 Bilton Road, POB 100, Somers, CT 06071'),
+    ('Robinson Correctional Institution', 'Robinson CI', '285 Shaker Road, POB 1400, Enfield , CT 06082'),
+    ('Willard-Cybulski Correctional Institution / Cybulski Community Reintegration Center', 'Willard-Cybulski CI', '391 Shaker Road, Enfield , CT 06082'),
+    ('York Correctional Institution', 'York CI', '201 West Main Street, Niantic, CT 06357');

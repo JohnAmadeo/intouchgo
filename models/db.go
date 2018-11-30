515603e3b08c96371237fc648c72f4f3c28cb4d1
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2018-11-30 15:22:42 -0500

    Add logic to connect to DB in production

--- models/db.go
+++ models/db.go
@@ -2,18 +2,28 @@ package models
 
 import (
 	"database/sql"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 const (
-	Driver       = "postgres"
-	DBConnection = "user=johnamadeodaniswara dbname=intouch_android sslmode=disable"
+	Driver            = "postgres"
+	LocalDBConnection = "user=johnamadeodaniswara dbname=intouch_android sslmode=disable"
 )
 
 func getDBConnection() (*sql.DB, error) {
-	db, err := sql.Open(Driver, DBConnection)
+	var dataSource string
 
+	if dbURL, ok := os.LookupEnv("DATABASE_URL"); !ok {
+		dataSource = LocalDBConnection
+	} else {
+		// the choice to use the "DATABASE_URL" environment variable is detailed in
+		// https://devcenter.heroku.com/articles/heroku-postgresql#provisioning-heroku-postgres
+		dataSource = dbURL
+	}
+
+	db, err := sql.Open(Driver, dataSource)
 	if err != nil {
 		db.Close()
 		return db, err
